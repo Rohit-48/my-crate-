172f80acfef61c0867603e1449df04938d3f1470
Rohit <rohitmandavkar3577@gmail.com>
2026-02-28 15:26:38 +0530

    setup completed

---
+++ webhook/target/debug/deps/webhook-a64b048105fd619b.d
@@ -0,0 +1,5 @@
+/home/giyu/Dev/obsidian-publish/webhook/target/debug/deps/webhook-a64b048105fd619b.d: src/main.rs
+
+/home/giyu/Dev/obsidian-publish/webhook/target/debug/deps/libwebhook-a64b048105fd619b.rmeta: src/main.rs
+
+src/main.rs:
---
+++ webhook/target/debug/deps/webhook-a8cf41ad0463d133.d
@@ -0,0 +1,5 @@
+/home/giyu/Dev/obsidian-publish/webhook/target/debug/deps/webhook-a8cf41ad0463d133.d: src/main.rs
+
+/home/giyu/Dev/obsidian-publish/webhook/target/debug/deps/libwebhook-a8cf41ad0463d133.rmeta: src/main.rs
+
+src/main.rs:
